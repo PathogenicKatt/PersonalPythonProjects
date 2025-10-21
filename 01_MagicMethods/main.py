127f68d2f63da517a902a91af4a1c722f96e7aed
Katleho Mofokeng <katlehom814@gmail.com>
2025-10-21 15:39:31 +0200

    Add main.py

---
+++ 01_MagicMethods/main.py
@@ -0,0 +1,26 @@
+# Magic Methods
+
+class LocalUniversity:
+	def __init__(self, name, province, numb_of_grad, year):
+		self.name=name
+		self.province= province
+		self.numb_of_grad = numb_of_grad
+		self.year = year
+
+	def __str__(self):
+		return f'The {self.name} is located in {self.province} and had {self.numb_of_grad} graduates in {self.year}.'
+
+	def __contains__(self, keyword):
+		return keyword in self.name or keyword in self.province
+
+	def __eq__(self, other):
+		return self.name == other.name 
+
+university1 = LocalUniversity("NWU", "North-West", 1000, 2024)
+university2 = LocalUniversity("UJ", "Gauteng", 3000, 2024)
+university3 = LocalUniversity("Wits", "Gauteng", 900, 2024)
+
+
+print(university1) # __str__
+print("UJ" in university2) # __contains__
+print(university1 == university2) # __eq__
